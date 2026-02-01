b636139afcdcc2367b9aa2d47f32b3bf39c85c5e
GladVillainy <lucasbuades00@gmail.com>
2026-02-01 18:22:42 +0100

    Implement algorithms
    After creating the methods (last commit), I created the algorithms in class TaskList (the methods are not tested)

--- src/main/java/ek/dk/friday_2026_01_30/Main1.java
+++ src/main/java/ek/dk/friday_2026_01_30/Main1.java
@@ -2,6 +2,6 @@ package ek.dk.friday_2026_01_30;
 
 public class Main1 {
     public static void main(String[] args) {
-
+        
     }
 }
--- src/main/java/ek/dk/friday_2026_01_30/TaskList.java
+++ src/main/java/ek/dk/friday_2026_01_30/TaskList.java
@@ -1,9 +1,7 @@
 package ek.dk.friday_2026_01_30;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Spliterator;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.function.Consumer;
 
 
@@ -15,24 +13,49 @@ public class TaskList<T extends Task> implements  Iterable<T> {
     }
 
     public void filterTaskByKeywords(String keyWord){
-  
+        Collection<T> tasksByWords = tasks.stream()
+                .filter(tasks -> {
+                    if (tasks.getDescription().equals(keyWord)
+                            || tasks.getTitle().equals(keyWord)){
+                        return true;
+                }
+                return false;
+                })
+                .toList();
+        if(tasksByWords.isEmpty()){
+            System.out.println("The keyword do not match any task");
+        } else
+            tasksByWords.forEach(System.out::println);
     }
 
     public void sortTaskByDueDate(){
-
+        List<T> sortByDue = tasks.stream()
+                .sorted((t1, t2) -> t1.getDuedate().compareTo(t2.getDuedate()))
+                .toList();
+        sortByDue.forEach(System.out::println);
     }
 
     public void dueToday(){
+        LocalDate today = LocalDate.now();
 
+        List<T> filterbyDue = tasks.stream()
+                .filter(t -> t.getDuedate().equals(today))
+                .toList();
+        filterbyDue.forEach(System.out::println);
     }
 
     public void overdue(){
+        LocalDate today = LocalDate.now();
+
+        List<T> overdued = tasks.stream()
+                .filter(t -> today.isAfter(t.getDuedate()))
+                .toList();
+        overdued.forEach(System.out::println);
 
     }
 
-
     public void printAllTask(){
-
+        tasks.forEach(System.out::println);
     }
 
 
