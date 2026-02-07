f31e539a60505044883bfd1d62c6beee02392d4b
GladVillainy <lucasbuades00@gmail.com>
2026-02-07 15:40:59 +0100

    Decorator pattern object creation
    Did so you can make a cheese pizza by wrapping the object by using decorator pattern

--- src/main/java/ek/dk/friday_2026_02_6/PizzaDemo.java
+++ src/main/java/ek/dk/friday_2026_02_6/PizzaDemo.java
@@ -4,11 +4,24 @@ public class PizzaDemo {
     public static void main(String[] args) {
         PizzaFactory factory = new PizzaFactory();
 
-        factory.createPizza("Veggie");
-        factory.createPizza("Margherita");
+        //Factory pattern
+        Pizza veggie = factory.createPizza("Veggie");
+        Pizza margherita = factory.createPizza("Margherita");
 
-        // TODO: Create a pizza using the factory
-        // TODO: Add some toppings (decorators)
+        //Decorator pattern
+        Pizza pizza1 =
+                new PepperoniTopping(
+                new CheeseTopping(
+                        factory.createPizza("Margherita")));
+
+        Pizza pizza2 =
+                new CheeseTopping(
+                factory.createPizza("Veggie"));
+
+        System.out.println(pizza1.getDescription() +" $"+ pizza1.getCost());
+        System.out.println(pizza2.getDescription() +" $"+ pizza2.getCost());
+
+
         // TODO: Place an order with a delivery strategy
     }
 }
