4c4f2105aec6a8802b3e9ef2c3b6753c00567d13
GladVillainy <lucasbuades00@gmail.com>
2026-02-07 15:11:37 +0100

    Tested
    Tested the factory pattern, passed

--- src/main/java/ek/dk/friday_2026_02_6/PizzaDemo.java
+++ src/main/java/ek/dk/friday_2026_02_6/PizzaDemo.java
@@ -4,6 +4,9 @@ public class PizzaDemo {
     public static void main(String[] args) {
         PizzaFactory factory = new PizzaFactory();
 
+        factory.createPizza("Veggie");
+        factory.createPizza("Margherita");
+
         // TODO: Create a pizza using the factory
         // TODO: Add some toppings (decorators)
         // TODO: Place an order with a delivery strategy
